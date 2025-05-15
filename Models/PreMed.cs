e7377c5f3d0d5b4f24941972156ef9c4c199dda5
HishamHalabi <159281419+HishamHalabi@users.noreply.github.com>
2025-05-15 17:13:38 +0300

    Add files via upload

---
+++ Models/PreMed.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital.Models
+{
+    public class PreMed
+    { 
+
+        public string MedID { get; set; }
+        public string PreID { get; set; }
+
+        public string Dsoage { get; set; }
+        public string Quantity { get; set; }
+
+        public string NTimes { get; set; }
+
+        public Medicine Medicine { get; set; }
+        public Prescription Prescription { get; set; }
+    }
+}
